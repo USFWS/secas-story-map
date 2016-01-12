4de6ac0b4893057dc5f29cba26bfd727bcf7e4d7
Roy Hewitt <roy_hewitt@fws.gov>
2016-01-12 11:18:23 -0500

    Add dom helpers to util

--- app/js/util.js
+++ app/js/util.js
@@ -1,13 +1,52 @@
 (function () {
   'use strict';
 
+  require('classlist-polyfill');
+
   var _ = {
     defaults: require('lodash.defaults'),
     find: require('lodash.find')
   };
 
+  var dom = {
+    create: create,
+    remove: remove,
+    addClass: addClass,
+    removeClass: removeClass,
+    toggleClass: toggleClass
+  };
+
+  function create(tagName, className, container) {
+    var el = document.createElement(tagName);
+    el.className = className;
+
+    if (container)
+      container.appendChild(el);
+
+    return el;
+  }
+
+  function remove(el) {
+    var parent = el.parentNode;
+    if (parent)
+      parent.removeChild(el);
+  }
+
+  function addClass(el, name) {
+    el.classList.add(name);
+  }
+
+  function removeClass(el, name) {
+    el.classList.remove(name);
+  }
+
+  function toggleClass(el, name) {
+    el.classList.toggle(name);
+  }
+
   module.exports = {
-    _ : _
+    _ : _,
+    dom: dom
   };
 
 })();
