9a694b39ffb30734679f727a11ce2b783be09c16
Roy Hewitt <roy_hewitt@fws.gov>
2016-03-16 13:13:10 -0400

    Move array of splash images into it's own file

---
+++ app/js/splash-images.js
@@ -0,0 +1,53 @@
+(function () {
+  'use strict';
+
+  module.exports = [
+    {
+      "src": "apple-orchards-falls-virginia.png",
+      "caption": "Apple Orchards Falls, Virginia. Photo: Matthew Cimitile"
+    },
+    {
+      "src": "appalachian-trail.png",
+      "caption": "River off of the Appalachian Trail, Georgia. Photo: Matthew Cimitile"
+    },
+    {
+      "src": "appalachian-waterfall.png",
+      "caption": "Cascade Falls, Virginia. Photo: Matthew Cimitile"
+    },
+    {
+      "src": "black-mountain-park-georgia.png",
+      "caption": "Black Mountain Park, Georgia. Photo: Matthew Cimitile"
+    },
+    {
+      "src": "cape-lookout-lighthouse.png",
+      "caption": "Cape Lookout National Seashore, North Carolina. Photo: Joe Lamm, NPS"
+    },
+    {
+      "src": "paddling-canals-of-alligator-river.png",
+      "caption": "Paddling the canals of Alligator River National Wildlife Refuge.  Photo: Steve Hillerbrand, USFWS"
+    },
+    {
+      "src": "american-tobacco-trail.png",
+      "caption": "Fall foliage on the American Tobacco Trail in North Carolina. Photo: <a href='https://www.flickr.com/photos/mr_t_in_dc/'>Mr.TinDC</a> <a href='https://creativecommons.org/licenses/by-nd/2.0/'>CC BY-ND 2.0</a>",
+      "full": "https://flic.kr/p/pNUyw4"
+    },
+    {
+      "src": "water-trails-okefenokee.png",
+      "caption": "Okefenokee National Wildlife Refuges. Photo: <a href='https://www.flickr.com/photos/killkudzu/'>Michael Lusk</a> <a href='https://creativecommons.org/licenses/by-nc/2.0/'>CC BY-NC 2.0</a>",
+      "full": "https://flic.kr/p/r3ypp3"
+    },
+    {
+      "src": "lower-suwanee.png",
+      "caption": "Gulf side of Lower Suwanee National Wildlife Refuge.  Photo: George Willson"
+    },
+    {
+      "src": "st-simons-island.png",
+      "caption": "Salt marsh tidal river estuary at St. Simons Island, Georgia. Photo: George Gentry, USFWS"
+    },
+    {
+      "src": "slash-pine-flatwoods.png",
+      "caption": "Slash Pine (<em>Pinus elliottii</em>) flatwoods in Osceola National Forest. Photo:<a href='https://www.flickr.com/photos/79666107@N00/'>Chris M Morris</a> <a href='https://creativecommons.org/licenses/by/2.0/'>CC BY 2.0</a>",
+      "full": "https://flic.kr/p/9LmmDb"
+    }
+  ];
+})();
