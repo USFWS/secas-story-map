b39329b2461c2954ded5edd9ee914d62ad25f93f
Roy Hewitt <roy_hewitt@fws.gov>
2018-06-14 13:21:38 -0400

    Do not include random image

--- app/js/splash.js
+++ app/js/splash.js
@@ -1,54 +1,53 @@
-(function () {
-  'use strict';
+(function() {
+  "use strict";
 
-  var store = require('store');
+  var store = require("store");
 
-  var emitter = require('./mediator');
-  var dom = require('./util').dom;
-  var _ = require('./util')._;
-  var template = require('../templates/splash.jade');
-  var backgrounds = require('./splash-images');
+  var emitter = require("./mediator");
+  var dom = require("./util").dom;
+  var template = require("../templates/splash.jade");
+  var backgrounds = require("./splash-images");
 
   var options = {};
-  var activeClass = 'active';
+  var activeClass = "active";
   var active = true;
 
   function init() {
-    options.splash = dom.create('aside', 'splash-screen', document.body);
+    options.splash = dom.create("aside", "splash-screen", document.body);
     dom.addClass(options.splash, activeClass);
     options.splash.innerHTML = template();
-    options.background = dom.create('div', 'splash-background', document.body);
+    options.background = dom.create("div", "splash-background", document.body);
     dom.addClass(options.background, activeClass);
-    options.close = dom.create('button', 'splash-close', options.splash);
-    options.close.innerHTML = 'Close';
-    options.checkbox = document.querySelector('.dont-show-again');
-    if ( store.get('disableSplashScreen') ) close();
+    options.close = dom.create("button", "splash-close", options.splash);
+    options.close.innerHTML = "Close";
+    options.checkbox = document.querySelector(".dont-show-again");
+    if (store.get("disableSplashScreen")) close();
     registerHandlers();
-    randomImage();
+    // randomImage();
   }
 
-  function randomImage() {
-    var img = _.random(backgrounds);
-    var imgRoot = 'http://usfws.github.io/secas-story-map/images/splash-photos/';
-    var splash = document.querySelector('.splash-screen.active');
-    splash.style.backgroundImage = 'url(' + imgRoot + img.src + ')';
-    emitter.emit('splash:background', img);
-  }
+  // function randomImage() {
+  //   var img = _.random(backgrounds);
+  //   var imgRoot = 'http://usfws.github.io/secas-story-map/images/splash-photos/';
+  //   var splash = document.querySelector('.splash-screen.active');
+  //   if (splash) splash.style.backgroundImage = 'url(' + imgRoot + img.src + ')';
+  //   emitter.emit('splash:background', img);
+  // }
 
   function setLocalStorage() {
-    if (options.checkbox.checked) store.set('disableSplashScreen', true);
-    else store.set('disableSplashScreen', true);
+    if (options.checkbox.checked) store.set("disableSplashScreen", true);
+    else store.set("disableSplashScreen", true);
   }
 
   function registerHandlers() {
-    options.close.addEventListener('click', close);
-    options.checkbox.addEventListener('click', setLocalStorage);
-    options.background.addEventListener('click', close);
+    options.close.addEventListener("click", close);
+    options.checkbox.addEventListener("click", setLocalStorage);
+    options.background.addEventListener("click", close);
   }
 
   function destroy() {
-    options.close.removeEventListener('click', close);
-    options.background.removeEventListener('click', close);
+    options.close.removeEventListener("click", close);
+    options.background.removeEventListener("click", close);
   }
 
   function close() {
