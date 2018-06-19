c0bd2386b05bcdde7f96be0bfce322e84fa72780
Roy Hewitt <roy_hewitt@fws.gov>
2018-06-19 09:22:56 -0400

    Change conditions under which the gallery is opened and closed based on other components

--- app/js/gallery.js
+++ app/js/gallery.js
@@ -1,17 +1,17 @@
-(function () {
-  'use strict';
+(function() {
+  "use strict";
 
-  var imagesLoaded = require('imagesloaded');
+  var imagesLoaded = require("imagesloaded");
 
-  var ProjectService = require('./data-access');
+  var ProjectService = require("./data-access");
 
-  var _ = require('./util')._;
-  var template = require('../templates/gallery.jade');
-  var emitter = require('./mediator');
-  var dom = require('./util').dom;
+  var _ = require("./util")._;
+  var template = require("../templates/gallery.jade");
+  var emitter = require("./mediator");
+  var dom = require("./util").dom;
 
   var options,
-      gallery = false;
+    gallery = false;
 
   function init(opts) {
     options = opts;
@@ -22,18 +22,19 @@
   }
 
   function createGallery() {
-    options.el = dom.create('section', 'gallery-hidden', document.body);
-    options.close = dom.create('button', 'gallery-toggle', options.el);
-    options.close.setAttribute('aria-label', 'Close');
-    options.list = dom.create('ul', 'gallery-list', options.el);
-    dom.addClass(options.el, 'gallery-container');
+    options.el = dom.create("section", "gallery-hidden", document.body);
+    options.close = dom.create("button", "gallery-toggle", options.el);
+    options.close.setAttribute("aria-label", "Close");
+    options.list = dom.create("ul", "gallery-list", options.el);
+    dom.addClass(options.el, "gallery-container");
   }
 
   function registerHandlers() {
-    options.close.addEventListener('click', toggle);
-    options.list.addEventListener('click', itemHandler);
-    emitter.on('gallery:close', hide);
-    emitter.on('zoomtofullextent', show);
+    options.close.addEventListener("click", toggle);
+    options.list.addEventListener("click", itemHandler);
+    emitter.on("infowindow:close", show);
+    emitter.on("zoomtofullextent", show);
+    emitter.on("project:click", hide);
   }
 
   function render() {
@@ -44,13 +45,14 @@
 
   function show() {
     gallery = true;
-    options.el.classList.remove('gallery-hidden');
-    emitter.emit('infowindow:close', false);
+    options.el.classList.remove("gallery-hidden");
+    emitter.emit("gallery:open", true);
   }
 
   function hide() {
     gallery = false;
-    options.el.classList.add('gallery-hidden');
+    options.el.classList.add("gallery-hidden");
+    emitter.emit("gallery:close", true);
   }
 
   function toggle() {
@@ -59,12 +61,11 @@
 
   function itemHandler(e) {
     var el = e.target;
-    var projectName;
-    while (el && el.tagName !== 'LI') {
-       el = el.parentNode;
+    while (el && el.tagName !== "LI") {
+      el = el.parentNode;
     }
-    projectName = el.getAttribute('data-name');
-    emitter.emit('project:click', ProjectService.getProject(projectName));
+    var projectName = el.getAttribute("data-name");
+    emitter.emit("project:click", ProjectService.getProject(projectName));
   }
 
   module.exports.init = init;
