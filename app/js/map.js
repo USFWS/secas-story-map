e29cbd359cfac0ca604f352a826b23b760e3fa67
Roy Hewitt <roy_hewitt@fws.gov>
2023-10-26 11:24:10 -0400

    Update basemap from Stamen to ESRI

--- app/js/map.js
+++ app/js/map.js
@@ -63,10 +63,10 @@ function createZoomToFullExtent() {
 
 function addBasemap() {
   L.tileLayer(
-    "http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}",
+    "https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}",
     {
       attribution:
-        "Map tiles by <a href=\"http://stamen.com\">Stamen Design</a>, <a href=\"http://creativecommons.org/licenses/by/3.0\">CC BY 3.0</a> &mdash; Map data &copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>",
+        "Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC",
       subdomains: "abcd",
       minZoom: 0,
       maxZoom: 18,
